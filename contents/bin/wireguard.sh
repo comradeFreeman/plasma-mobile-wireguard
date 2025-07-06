caea290a192fb2aedd6bab09e66aef3ca998d136
comradeFreeman <mister.siroejkin@yandex.ru>
2025-07-06 18:04:29 +0300

    Translation #1

--- contents/bin/wireguard.sh
+++ contents/bin/wireguard.sh
@@ -3,20 +3,20 @@
 VPN_ID=`nmcli -t c show | grep '^[^:]*:[^:]*:wireguard:[^:]*$' | head -n 1 | cut -d : -f 1`
 if nmcli -t c show "$VPN_ID" | grep -q '^GENERAL.STATE:activated$' ; then
   nmcli c down "$VPN_ID"
-  # Notification
+  kdialog --passivepopup "Wireguard profile '$FILE' successfully deactivated" 5
 else
   if [ ! -z "$VPN_ID" ] ; then
     nmcli c up "$VPN_ID"
-    # Notification
+    kdialog --passivepopup "Wireguard profile '$FILE' successfully activated" 5
   else
     mkdir -p /etc/wireguard
     for f in /etc/wireguard/*-*; do [ -e "$f" ] && mv -- "$f" "${f//-/}"; done
     FILE=`ls /etc/wireguard | ls -tp | grep -v '/$' | head -n 1`
     if [ ! -z "$FILE" ] ; then
       nmcli connection import type wireguard file /etc/wireguard/$FILE
-      kdialog --passivepopup "Импортирован профиль Wireguard $FILE" 5
+      kdialog --passivepopup "Wireguard profile '$FILE' imported" 5
     else
-      if [ -z "$1" ] && kdialog --yesno "Настроенные профили wireguard не найдены. Вы можете добавить профиль вручную или положить конфиг по пути /etc/wireguard/. Нажмите 'ОК', чтобы открыть настройки сети и 'Отмена', чтобы выйти" --title "Конфиг не найден"; then
+      if [ -z "$1" ] && kdialog --yesno "No configured Wireguard profiles found. You can add the profile manually or put the config in the /etc/wireguard/. Click 'Yes' to open network settings and 'No' to exit" --title "Profiles not found"; then
         kcmshell6 networkmanagement && exec "$0" stop
       fi
     fi
